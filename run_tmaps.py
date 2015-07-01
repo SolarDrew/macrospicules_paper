2231306f93fcea41f43294b99cdc5eaf2a73dfdc
Andrew Leonard <andy.j.leonard@gmail.com>
2015-07-01 18:39:55 +0100

    Terrible terrible workaround so it doesn't break

--- run_tmaps.py
+++ run_tmaps.py
@@ -8,13 +8,20 @@ from temperature import TemperatureMap as TMap
 from matplotlib import pyplot as plt
 from sunpy.time import parse_time as parse
 
-dates = ['2014-12-15 12:00']
+dates = ['2014-12-15 12:00', '2014-12-15 12:30', '2014-12-15 14:00']
 
-for date in dates:
-    thismap = TMap(date, data_dir=join(CThome, 'data'), maps_dir=CThome, verbose=True)
-    thismap.save()
-    fig = plt.figure(figsize=(32, 24))
-    thismap.plot()
-    plt.colorbar()
-    plt.savefig('t_{:%Y-%m-%d_%H-%M-%S}'.format(parse(thismap.date)))
-    plt.close()
+completed = False
+while completed == False:
+    try:
+        for date in dates:
+            thismap = TMap(date, data_dir=join(CThome, 'data'), maps_dir=CThome, verbose=True)
+            thismap.save()
+            #thismap = thismap.submap([-1200, -200], [-500, 500], units='data')
+            fig = plt.figure(figsize=(32, 24))
+            thismap.plot()
+            plt.colorbar()
+            plt.savefig('t_{:%Y-%m-%d_%H-%M-%S}'.format(parse(thismap.date)))
+            plt.close()
+        completed = True
+    except:
+        print 'Failed - restarting'
